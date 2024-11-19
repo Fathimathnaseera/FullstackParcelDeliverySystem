390ddfc3bd1178931e1eeb255d8ee9d3249c3c2b
fathimath naseera <fathimathanassera2003@gmail.com>
2024-11-19 12:40:21 +0530

    Authentication have been created

---
+++ Backend/models/Parcel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema(
+    {
+    from:{type:String, required:true},
+    to:{type:String, required:true},
+    sendername:{type:String, required:true},
+    recipientname:{type:String, required:true},
+    senderemail:{type:String, required:true},
+    recipientemail:{type:String, required:true},
+    weight:{type:Number, required:true},
+    cost:{type:Number, required:true},
+    date:{type:String, required:true},
+    note:{type:String},
+    feedback:{type:String},
+    status:{type:Number, default:0},
+},
+ {
+    timestamp:true,
+ }
+);
+
+
+module.exports = mongoose.model("parcel", parcelSchema)
